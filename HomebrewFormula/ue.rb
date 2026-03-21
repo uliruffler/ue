8cf52c2db87971903858d9241e4afa2fec2e6312
ruffler <uli.ruffler@gmail.com>
2026-03-21 11:01:01 +0100

    Homebrew formula: update to v0.1.4

--- HomebrewFormula/ue.rb
+++ HomebrewFormula/ue.rb
@@ -1,8 +1,8 @@
 class Ue < Formula
   desc "Simple terminal text editor with configurable key bindings"
   homepage "https://github.com/uliruffler/ue"
-  url "https://github.com/uliruffler/ue/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "c9950063545f5f286f8bf2a308868e35961a7c02338a77a3300144a06ed104e5"
+  url "https://github.com/uliruffler/ue/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "0bacc68bff8b782c3012b475e73eedf2a019f89192bc8bacf4425231d301bcb8"
   license "GPL-3.0-only"
   head "https://github.com/uliruffler/ue.git", branch: "main"
 
